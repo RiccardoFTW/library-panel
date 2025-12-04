e0a721976ce6f4e94710b24243e1daa9c54181b5
Micio Dev <micio86dev@gmail.com>
2025-12-04 18:09:48 +0000

    feat: tailwind 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const contatore = reactive({ a: { x: 4 }, b: 0 })
+
+const increment = () => {
+  contatore.a.x = 10
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="card">
+    <button @click="increment">+ {{ contatore.a.x }}</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@reference "tailwindcss";
+
+.card {
+  @apply p-4 bg-red-500;
+
+  h1 {
+    @apply text-xl font-bold;
+  }
+
+  .btn {
+    @apply mt-2 text-blue-500;
+  }
+}
+</style>
