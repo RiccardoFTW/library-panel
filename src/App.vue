aba279e1c06ecbe388b4be50e49192864f7d2bbf
Micio Dev <micio86dev@gmail.com>
2025-12-18 17:18:06 +0000

    fix: after merge

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
 <template>
-<<<<<<< HEAD
-  <RouterView />
-=======
   <router-view />
->>>>>>> cc0d9e5276f2aefae114522bbf82aeb498f97e87
 </template>
--- src/components/molecules/FormLogin.vue
+++ src/components/molecules/FormLogin.vue
@@ -34,7 +34,6 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-<<<<<<< HEAD
   <form @submit.prevent="handleSubmit" class="w-full p-8">
     <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Accedi</h2>
 
@@ -43,18 +42,6 @@ const handleSubmit = async () => {
     </div>
 
     <InputField v-model="email" type="email" label="Email" placeholder="Inserisci la tua email" />
-=======
-  <form class="w-full max-w-md bg-white p-8 rounded-lg shadow-md" @submit.prevent="login">
-    <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Accedi</h2>
-
-    <InputField type="email" label="Email" placeholder="Inserisci la tua email" v-model="email" />
-    <InputField
-      type="password"
-      label="Password"
-      placeholder="Inserisci la tua password"
-      v-model="password"
-    />
->>>>>>> cc0d9e5276f2aefae114522bbf82aeb498f97e87
 
     <InputField
       v-model="password"
@@ -66,25 +53,3 @@ const handleSubmit = async () => {
     <Button type="submit" :text="loading ? 'Caricamento...' : 'Accedi'" />
   </form>
 </template>
-
-<<<<<<< HEAD
-=======
-<script setup lang="ts">
-import { ref, reactive } from 'vue'
-import InputField from '../atoms/InputField.vue'
-import Button from '../atoms/ButtonForm.vue'
-
-const formData = reactive({
-  email: ref('email'),
-  password: ref('password'),
-})
-
-const login = () => {
-  // Service chiama la funzione login verso un'api server
-  console.log(JSON.stringify(formData), 'formData')
-  alert('Hai inviato i dati di login!')
-}
-</script>
-
->>>>>>> cc0d9e5276f2aefae114522bbf82aeb498f97e87
-<style scoped></style>
